a4bece7be701157b2bed36fc3de214ec912b0171
ausername-1 <81475574+ausername-1@users.noreply.github.com>
2021-04-03 01:10:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const axios = require('axios');
+const fetch = require('node-fetch');
 
 
 module.exports = {
-	stats: async function() {
-		let stats = await axios.get('https://discord.riverside.rocks/stats.json.php')
-		return stats.data;
-	},
-
-	checkuser:  async function(id) {
-		if (typeof userid !== 'string') throw new TypeError("User ID isn't valid");
-			let checkUser = await axios.get(`https://discord.riverside.rocks/check.json.php?id=${id}`)
-         	return checkUser.data;
+    stats: async function() {
+        const stats = await fetch('https://discord.riverside.rocks/stats.json.php').then((res => res.json()))
+        return stats;
     },
-	
-	reportuser: async function (key, id, details) {
-		if (typeof userid !== 'string') throw new TypeError("User ID isn't valid");
-			let reportUser = await axios.get(`https://discord.riverside.rocks/report.json.php?key=${key}&id=${id}&details=${details}`)
-			return reportUser.data;
+    
+    checkuser:  async function(id) {
+     if (typeof id !== 'string') throw new TypeError("User ID isn't valid");
+      const usertocheck = await fetch(`https://discord.riverside.rocks/check.json.php?id=${id}`).then((res => res.json()))
+     return usertocheck;
     },
-	
-	deleteallreports: async function (key) {
-		let dataToDelete await axios.get(`https://discord.riverside.rocks/delete.json.php?key=${key}`)
-        return dataToDelete.data;
+    
+    reportuser: async function (key, id, details) {
+        if (typeof id !== 'string') throw new TypeError("User ID isn't valid");
+    
+        const report = await fetch(`https://discord.riverside.rocks/report.json.php?key=${key}&id=${id}&details=${details}`).then((res => res.json()))
+        return report;
+    },
+    
+    deleteallreports: async function (key) {
+     const datatodelete = await fetch(`https://discord.riverside.rocks/delete.json.php?key=${key}`)
+     return datatodelete;
     }
 }
